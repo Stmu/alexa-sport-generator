d5be69949f2e6c2360428ca20ab9d87ffbc37d38
stefan <stmu@gmx.net>
2017-12-28 14:02:29 +0100

    ..

--- index.js
+++ index.js
@@ -1,122 +1,58 @@
 "use strict";
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 var SKILL_STATES = {
-    QUESTIONS: "_QUESTIONMODE", // Asking questions.
-    START: "_STARTMODE", // Entry point, start the game.
+    SPORT: "_STARTMODE",
     HELP: "_HELPMODE" // The user is asking for help.
 };
 
-var animalSound = {
-    kuh: {sound: "muhmuuuu mhuuu muuuu", article: 'die'},
-    katze: {sound: "miau miau", article: 'die'},
-    schwein: {sound: "oink oink", article: 'das'},
-    huhn: {sound: "borg borg ", article: 'das'},
-    hund: {sound: "wau wau wau", article: 'der'},
-    pferd: {sound: "ühhhüühüüü", article: 'das'},
-    frosch: {sound: "quak quak quak", article: 'der'},
-    elefant: {sound: "töröö", article: 'der'},
-    biene: {sound: "wau wau wau", article: 'die'},
-    hummel: {sound: "zzzz", article: 'die'},
-    maus: {sound: "piep piep", article: 'die'},
-    uhu: {sound: "schuuhuuuhu schuuhuu", article: 'der'},
-    eule: {sound: "schuuhuuuhu schuuhuu", article: 'die'},
-    meerschwein: {sound: "quiek quiek", article: 'das'},
-    kücken: {sound: "schiep schiep", article: 'das'},
-    esel: {sound: "iaaaa iaaaaa", article: 'der'},
-    ente: {sound: "nak nak nak", article: 'die'},
-    vogel: {sound: "piep piep piep", article: 'der'},
-    schaf: {sound: "määä määäh", article: 'das'},
-    wal: {sound:'uuuuuuuuuuuuuuuuuuuu', article:'der'}
-}
+var exercises = [
+    "kniebeuge",
+    "liegestütze",
+    "sit ups",
+    "rumpfbeugen"
+]
+
+var greetings = [
+    "Hallo ich helfe dir fit zu bleiben. Dazu mach %s %s.",
+    "Ich helfe dir fit zu werden. Dazu führe %s %s aus.",
+    "Führe %s %s aus. Das hält dich fit",
+]
 
 var Alexa = require("alexa-sdk");
+var util = require('util');
 
 exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
-    alexa.registerHandlers(newSessionHandlers, startHandler, questionHandler);
+    alexa.registerHandlers(newSessionHandlers, sportHandler);
+    
     alexa.execute();
 };
 
-var startHandler = Alexa.CreateStateHandler(SKILL_STATES.START, {
-    "Intro": function () {
-        var intent = this.event.request.intent
-        if(intent === undefined){
-            this.handler.state = SKILL_STATES.QUESTIONS;
-            this.emit(':ask', "Hallo ich kann Tierstimmen nachmachen. Frage mich dazu beispielsweise: Wie macht die Katze?");
-        }
-        
-        this.emitWithState("QuestionIntent", true);
-    }
+var sportHandler = Alexa.CreateStateHandler(SKILL_STATES.SPORT, {
+    "exercise": function () {
+      
+        var greetingsNumber = Math.floor((Math.random() * greetings.length) + 1);
+        var exerciseNumber = Math.floor((Math.random() * exercises.length) + 1);
+        var repetition = Math.floor((Math.random() * 10) + 1);
+       
+        var speechout = util.format(greetings[greetingsNumber], repetition, exercises[exerciseNumber]); 
+        console.log(speechout);
+        this.emit(':tell', speechout);
+    }   
 });
-
-const questionHandler = Alexa.CreateStateHandler(SKILL_STATES.QUESTIONS, {
-    'QuestionIntent': function (value) {
-
-        //emit response directly
-        var intent = this.event.request.intent
-        var animal = intent && intent.slots && intent.slots.Animal && intent.slots.Animal.value;
-        var article = intent && intent.slots && intent.slots.article && intent.slots.article.value;
-
-
-        if (animal !== undefined) {
-            if (animal === 'nein') {
-                this.emit(':tell', "Alles klar, bis bald.");
-            } else {
-                var sound = animalSound[animal]
-                console.log(sound);
-                console.log(intent);
-                if (article === undefined){
-                    article = sound.article;
-                }
-                if (sound !== undefined) {
-                    this.emit(':ask', sound.sound + " macht " + article + " " + animal + ". Frag mich nach einem anderen Tier oder sage Stop zum beenden.");
-                }
-                else {
-                    console.dir("unknown animal sound detected. Plase add sound for '" + animal + "'");
-                    this.emit(':ask', "Ich weiß leider nicht welchen Laut das Tier von sich gibt. Aber ich lerne immer weiter. Frag mich bald danach. Welches Tier möchtest du nun hören?");
-                }
-
-            }
-        }
-        else {
-            console.dir("unknown animal detected. Add '" + intent + "'");
-            this.emit(':ask', "Das Tier kenne ich noch nicht, aber ich lerne immer weiter. Frag mich bald danach. Welches Tier möchtest du jetzt hören?");
-        }
-
-        //  this.emit(":tell", "muuuuuhhhh");
-    },
-    "AMAZON.StartOverIntent": function () {
-        this.handler.state = SKILL_STATES.START;
-        this.emitWithState("Intro", false);
-    },
-    "AMAZON.HelpIntent": function () {
-        this.handler.state = SKILL_STATES.START;
-        this.emitWithState("Intro", false);
-    },
-    "AMAZON.StopIntent": function () {
-
-        this.emit(":tell", "bis bald...");
-    },
-    "AMAZON.CancelIntent": function () {
-        this.emit(":tell", "OK, dann bis bald mal wieder.");
-    },
-    "SessionEndedRequest": function () {
-        console.log("Session ended in trivia state: " + this.event.request.reason);
-    }
-});
-
 
 const newSessionHandlers = {
     'LaunchRequest': function () {
-        this.handler.state = SKILL_STATES.START;
-        this.emitWithState("Intro");
+        this.handler.state = SKILL_STATES.SPORT;
+        this.emitWithState("exercise");
     },
     "AMAZON.HelpIntent": function () {
-        this.emit(":tell", "Nachdem du das Skill mit Alexa, starte wie macht, gestartet hast, kannst du beispielsweise fragen, wie macht die Katze oder wie macht der Esel. Zum beenden sage einfach Stop oder Abbrechen.");
+        this.emit(":tell", "");
     },
     "Unhandled": function () {
-        this.emit(":tell", "Das habe ich nicht verstanden. Nachdem du das Skill mit Alexa, starte wie macht, gestartet hast, kannst du beispielsweise fragen, wie macht die Katze oder wie macht der Esel. Zum beenden sage einfach Stop oder Abbrechen.");
+        this.handler.state = SKILL_STATES.SPORT;
+        this.emitWithState("exercise");
     },
 
 };
